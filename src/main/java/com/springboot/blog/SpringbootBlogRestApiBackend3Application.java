8725247078939b165d630ff04330d0e68bd1877e
unknown <vijay516kumar@gmail.com>
2024-01-29 20:56:21 +0530

    base code

---
+++ src/main/java/com/springboot/blog/SpringbootBlogRestApiBackend3Application.java
@@ -0,0 +1,13 @@
+package com.springboot.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogRestApiBackend3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogRestApiBackend3Application.class, args);
+	}
+
+}
